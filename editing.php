fb4bb926ff931e2dcc1c22a9a6216c16069fcb2d
{s.komai} <{s.komai@seeds-std.co.jp}>
2024-07-31 15:32:54 +0900

    htmlspecialcharsの追加

--- editing.php
+++ editing.php
@@ -79,11 +79,11 @@ $_SESSION['token'] = $token;
                 <tbody>
                 <tr>
                     <th><label for="name">名前</label></th>
-                    <td><input type="text" name="name" id="name" value="<?= $checkid['name'] ?>" required></td>
+                    <td><input type="text" name="name" id="name" value="<?= htmlspecialchars($checkid['name']) ?>" required></td>
                 </tr>
                 <tr>
                     <th><label for="content">投稿内容</label></th>
-                    <td><textarea name="content" id="content" rows="4" required><?= $checkid['content'] ?></textarea></td>
+                    <td><textarea name="content" id="content" rows="4" required><?= htmlspecialchars($checkid['content']) ?></textarea></td>
                 </tr>
                 </tbody>
             </table>
